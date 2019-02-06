b7b977cd923279c4738399077815c0068ddc64ef
Untitled <littleluluno01@163.com>
2019-02-06 23:25:30 +0800

    add pagination support

--- src/fetch.ts
+++ src/fetch.ts
@@ -7,11 +7,13 @@ import { BAN_INDEX } from './config'
 
 /**
  * @description Fetch ban announcements.
+ * @param startIndex Optional. The index of announcement to start with. (For pagination)
  * @param maxAnnouncement Optional. Max number of announcements to be fetched.
  * @param maxConcurrency Optional. Max number of concurrent tasks.
  * @param indexUrl Optional. URL of ban announcements index.
  */
 export async function fetch(
+  startIndex: number = 0,
   maxAnnouncement: number = 0,
   maxConcurrency: number = 5,
   indexUrl: string = BAN_INDEX
@@ -21,6 +23,10 @@ export async function fetch(
   let announcements: Announcement[] = []
   let i = 0
   for(let annMeta of getAnnList(html)) {
+    if(i < startIndex) {
+      i++
+      continue
+    }
     // Push task
     announcements.push(annMeta)
     let index = i
@@ -30,6 +36,10 @@ export async function fetch(
     })
     i++
     if(maxAnnouncement && i >= maxAnnouncement) break
+  }
+  if(tasks.length == 0) return {
+    anns: [],
+    errs: []
   }
   // Start task
   let queue = new TaskQueue<void>(tasks, maxConcurrency)
